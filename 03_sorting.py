7c8c504697bcb55ebd61d57653a0731b59494a99
Aaron Nagel <49287509+nagaaron@users.noreply.github.com>
2020-05-01 13:06:28 +0200

    Add files via upload

--- 03_sorting.py
+++ 03_sorting.py
@@ -1,16 +1,17 @@
 # binary search O(logn), search O(n)
 
-#Insertion Sort 
+#Insertion Sort O(n^2)
 
-arr = [5,2,4,6,1,3,8,6,9,0]
+arr = [11,22,33,44,55,5,2,4,6,1,3,8,6,9,0]
+def insertion_sort(arr):
+    for index in range(len(arr)-1):
+        for indexmin in range(len(arr[index::-1])):
+            if arr[index+1]<arr[indexmin]:
+                arr[indexmin],arr[index+1] = arr[index+1],arr[indexmin]
+    return arr
 
-for index in range(len(arr)-1):
-    for indexmin in range(len(arr[index::-1])):
-        if arr[index+1]<arr[indexmin]:
-            arr[indexmin],arr[index+1] = arr[index+1],arr[indexmin]
-print(arr)
 
-#Insert Sort with binary search
+#Insert Sort with binary search O(log(n)n)
 def binary_search_sort(arr):
     for index in range(len(arr)-1):
         halfcheck = (index+1)//2
@@ -24,5 +25,50 @@ def binary_search_sort(arr):
                     arr[indexmin],arr[index+1] = arr[index+1],arr[indexmin]
     return arr
         
-        
-print(binary_search_sort(arr))  
+
+#because of auxillary space insertion is preferable over merge sort
+#merge sort O(log(n)n)
+
+def merge(arr,leftarr,rightarr):
+    
+    lenghtl = len(leftarr)
+    lenghtr = len(rightarr)
+    k,i,j  =0,0,0
+    while i < lenghtl and j < lenghtr:
+        if leftarr[i] <= rightarr[j]:
+            arr[k] = leftarr[i]
+            k+=1
+            i+=1
+        elif leftarr[i] > rightarr[j]:
+            arr[k] = rightarr[j]
+            k+=1
+            j+=1
+    while i <lenghtl:
+        arr[k] =leftarr[i]
+        k+=1
+        i+=1
+    while j <lenghtr:
+        arr[k] = rightarr[j]
+        k+=1
+        j+=1
+    return arr
+    
+def merge_sort(arr):
+    lenght = len(arr)
+    if lenght < 2:
+        return arr
+    mid = lenght//2
+    leftarr = arr[:mid]
+    rightarr = arr[mid:]
+    merge_sort(leftarr)
+    merge_sort(rightarr)
+    return merge(arr,leftarr,rightarr)
+
+
+print(insertion_sort(arr))
+print(binary_search_sort(arr))
+print(merge_sort(arr))
+
+
+
+
